82d8ad2dc05bd1eca7aa70ca03c4678a9941f2fb
venkat2509 <venkatms2509@gmail.com>
2018-07-20 21:09:32 -0400

    frosted glass effect on signup click
    On clicking register button frosted glass effect is ensued.

--- GoodDogs/app/assets/javascripts/application.js
+++ GoodDogs/app/assets/javascripts/application.js
@@ -10,7 +10,22 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(document).ready(function(){
+    console.log('Working');
+});
+
+$(function() {
+	$('[data-popup-open]').on('click', function(e) {
+		var targeted_popup_class = jQuery(this).attr('data-popup-open');
+		$('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+        e.preventDefault();
+        $('.before-content').addClass('content'); //adding frosted glass effect
+        $('.login-form-container').toggle();
+    });
+    
+});

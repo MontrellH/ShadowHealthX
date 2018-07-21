4dc0c49239d9e8512a5316a14e67f027c3fb9064
venkat2509 <venkatms2509@gmail.com>
2018-07-21 18:49:51 -0400

    Changes to style for adding logo, header and registration popup
    Adding error button username password registration popup and so on.

--- GoodDogs/app/assets/javascripts/application.js
+++ GoodDogs/app/assets/javascripts/application.js
@@ -28,5 +28,59 @@ $(function() {
         $('.login-form-container').toggle();
         $('.registration-popup').css("display","block");
     });
+
+    $('.login-form-input').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+    var input = $('.login-form-input-validate .login-form-input');
+    $('.login-form').on('submit',function(){    
+        var check = true;
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+        $.post('/sessions');
+        return check;
+    });
+
+
+    $('.login-form .login-form-input').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
     
 });

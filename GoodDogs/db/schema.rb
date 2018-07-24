7ce71a95012b3de8eaafbbc406b910dc834564a2
Sachin Paryani <sachin.paryani@gmail.com>
2018-07-24 10:28:29 -0400

    move google api script

--- GoodDogs/db/schema.rb
+++ GoodDogs/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_052617) do
+ActiveRecord::Schema.define(version: 2018_07_23_201528) do
+
+  create_table "shelters", force: :cascade do |t|
+    t.string "Address"
+    t.string "City"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "Name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"

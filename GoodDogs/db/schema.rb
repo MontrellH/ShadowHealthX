d457ee29e193a7abfe3012026dbdb62f87a730ac
venkat2509 <venkatms2509@gmail.com>
2018-07-26 09:59:59 -0400

    css and dropdown loading dynamically
    Dropdown loading dynamically.

--- GoodDogs/db/schema.rb
+++ GoodDogs/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_201528) do
+ActiveRecord::Schema.define(version: 2018_07_24_193908) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string "Address"

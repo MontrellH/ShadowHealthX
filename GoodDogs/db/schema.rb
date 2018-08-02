c838282be66f55dca294bcb288f74db71882e3ef
Montrellh <montrellharrington@yahoo.com>
2018-08-02 14:55:01 -0400

    Added carousel to shelter

--- GoodDogs/db/schema.rb
+++ GoodDogs/db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_08_01_150916) do
-
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
